094d957e9a1daa80ede761bcd078d50a617df49b
rafabra988 <44869209+rafabra988@users.noreply.github.com>
2021-10-16 21:48:57 -0300

    Add files via upload

--- questao_4/main.c
+++ questao_4/main.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    int vert1, vert2, peso, eh_digrafo=0, i, pai[6];
+    int eh_digrafo=0, pai[6],mat[6][6],i=0, j=1;
     Grafo *gr=cria_Grafo(6,6,1);
 
     FILE *arquivo = fopen("Comma Separated Values - grafo.csv","r");
@@ -15,8 +15,26 @@ int main()
     }
 
     while(!feof(arquivo)){
-        fscanf(arquivo, "%d,%d,%d", &vert1, &vert2, &peso);
-        insereAresta(gr,vert1,vert2,eh_digrafo,peso);
+        fscanf(arquivo, "%d,%d,%d,%d,%d,%d,%d", &mat[i][0],&mat[i][1],&mat[i][2],&mat[i][3],&mat[i][4],&mat[i][5],&mat[i][6]);
+        i++;
+    }
+
+//    for(i=0;i<6;i++){
+//        for(j=0;j<6;j++){
+//            printf("%d ",mat[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    printf("\n");
+
+    i=2;
+    while(j<6){
+        for(int h=i;h<7;h++)
+            insereAresta(gr, h-1,j-1,eh_digrafo,mat[h][j]);
+            //printf("(%d-%d %d)",h-1,j-1, mat[h][j]);
+//        printf("\n");
+        i+=1;
+        j+=1;
     }
 
     algPRIM(gr,0,pai);
